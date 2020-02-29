d91413f50509ff34d49767b171b05cd64441d49c
nikwalia <nikash.walia@gmail.com>
2020-02-29 15:59:05 -0600

    Temp commit

--- stat_helper.py
+++ stat_helper.py
@@ -18,26 +18,11 @@ def calc_min(df):
     return df.min()
 
 def smooth_values(df):
+    filtered = pd.DataFrame()
     for channel in df.columns:
-        df[channel] = gaussian_filter1d(df[channel].to_numpy(), 5)
+        filtered[channel] = gaussian_filter1d(df[channel].to_numpy(), 1)
     
-    first_quartiles = df.quantile(q = 0.25, axis = 0)
-    third_quartiles = df.quantile(q = 0.75, axis = 0)
-    iqr = third_quartiles - first_quartiles
-
-    for channel in df.columns:
-        df[channel].loc[df[channel] > third_quartiles[channel] + 1.5 * iqr[channel]] = np.NAN
-        df[channel].loc[df[channel] < first_quartiles[channel] - 1.5 * iqr[channel]] = np.NAN
-
-    df_interpol = df_interpol.interpolate().replace(np.NAN, 0)
-    return df_interpol
-
-def get_outliers(df):
-    first_quartiles = df.quantiles(q = 0.25)
-    third_quartiles = df.quantiles(q = 0.75)
-    iqr = third_quartiles - first_quartiles
-    outliers = df.loc[(df < first_quartiles - 1.5 * iqr) & (df > third_quartiles + 1.5 * iqr)]
-    return outliers
+    return filtered
 
 
 def get_stats_list(df):
