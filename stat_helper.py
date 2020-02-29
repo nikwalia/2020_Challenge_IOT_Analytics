ca4b4c4b5f7b19fbdad05341cc10904efbfc02e9
nikwalia <nikash.walia@gmail.com>
2020-02-29 09:23:35 -0600

    Work in progress cleansing data

--- stat_helper.py
+++ stat_helper.py
@@ -1,23 +1,23 @@
 import pandas as pd
 
 def calc_std(df):
-    return df.std(axis = 1)
+    return df.std(axis = 0)
 
 def calc_mean(df):
-    return df.mean(axis = 1)
+    return df.mean(axis = 0)
 
 def calc_max(df):
-    return df.max(axis = 1)
+    return df.max(axis = 0)
 
 def calc_min(df):
-    return df.min(axis = 1)
+    return df.min(axis = 0)
 
 def calc_median(df):
-    return df.median(axis = 1)
+    return df.median(axis = 0)
 
-def find_outliers(df):
-    first_quartiles = df.quantiles(q = 0.25, axis = 1)
-    third_quartiles = df.quantiles(q = 0.75, axis = 1)
+def remove_outliers(df):
+    first_quartiles = df.quantile(q = 0.25, axis = 0)
+    third_quartiles = df.quantile(q = 0.75, axis = 0)
     iqr = third_quartiles - first_quartiles
-    outliers = df.loc[df > third_quartiles + 1.5 * iqr | df < first_quartiles - 1.5 * iqr]
-    print(outliers)
+    non_outliers = df.loc[(df >= first_quartiles - 1.5 * iqr) & (df <= third_quartiles + 1.5 * iqr)]
+    return non_outliers
